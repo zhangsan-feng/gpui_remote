a303f02b87e86e629b85a4b68f3cf6952898cde7
流年 <1046364655@qq.com>
2026-09-15 11:37:12 +0800

    fix: hide windows console in debug builds

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-#![cfg_attr(
-    all(target_os = "windows", not(debug_assertions)),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 pub(crate) mod application;
 mod build_info;
